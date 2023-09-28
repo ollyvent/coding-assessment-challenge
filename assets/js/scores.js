589378dfdc08f90de042a336d9a58c7dbb8d4606
unknown <diloch111@gmail.com>
2023-09-28 19:47:08 +0100

    add function to save high scores to loacl storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,14 @@
-// grab high scores from the local storage
+// grab high scores from the local storage using a function
 function getHighScores() {
     var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
     return highScores;
 }
+
+// save high scores to local storage using a function
+function saveHighScores(initials, score) {
+    var highScores = getHighScores();
+    highScores.push({initials: initials, score: score });
+    highScores.sort((a, b) => b.score - a.score); // sort highscores in descending order
+    highScores = highScores.slice(0, 5); // keep only the top 5 scores
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+};
