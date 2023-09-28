be3535640b0a8ab2efef8b8ddfa9c4195ddbfa42
unknown <diloch111@gmail.com>
2023-09-28 19:13:49 +0100

    add questiona array

---
+++ assets/js/scores.js
@@ -0,0 +1,19 @@
+// create an array of objects to ask questions
+
+var questions = [
+    {
+        question: "what is javascript?",
+        choices: ["a programming language,", "a type of data", "a computer operator"],
+        correctAnswer: 0,
+    },
+    {
+        question: "which of the following is not a javascript data type?",
+        choices: ["string,", "boolean", "float", "number"],
+        correctAnswer: 2,
+    },
+    {
+        question: "what is the purpose of the 'if' statement in javascript?",
+        choices: ["declaring a variable,", "executing code conditionally", "creating loops"],
+        correctAnswer: 1,
+    },
+]
