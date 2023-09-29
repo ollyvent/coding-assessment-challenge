40094b48fb322ebd5013cff95d7496115eacc088
unknown <diloch111@gmail.com>
2023-09-29 07:30:42 +0100

    set up timer for the logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+// set up timer
+var timerInterval;
+var timeLeft = 60; // total time in seconds
+var currentQuestionIndex = 0;
+var score = 0;
+
+var startButton = document.getElementById("start");
+var questionTitile = document.getElementById("question-title");
+var choiceContainer = document.getElementById("choice");
+var timerElement = document.getElementById("title");
+
+function startQuiz() {
+    startButton.style.display = "none";
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
+
+    
+}
